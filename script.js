d95e54b23eacf3e109c14eda510b0798ab07457f
babik234 <85606809+babik234@users.noreply.github.com>
2024-01-07 01:14:08 +0100

    fgh

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let images = [
+    'https://cdn.pixabay.com/photo/2018/03/22/02/37/background-3249063_960_720.png',
+    'https://cdn.pixabay.com/photo/2016/03/27/18/54/technology-1283624_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2015/04/23/17/41/node-js-736399_960_720.png',
+    'https://cdn.pixabay.com/photo/2016/09/08/04/12/programmer-1653351_960_720.png'
+  ];
+let image = document.getElementById('');
+
 let x = 0
 let button  = document.getElementById("button")
 
